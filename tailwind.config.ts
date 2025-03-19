6aee7031ecdf6adcef184443f144d751e8b42430
박주원 <qkrwndnjs7308@gmail.com>
2025-03-19 14:33:26 +0900

    refactor :: tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
-import type { Config } from "tailwindcss";
-import tailwindcssAnimate from "tailwindcss-animate";
+import type { Config } from "tailwindcss"
 
 const config = {
   darkMode: ["class"],
@@ -235,7 +234,8 @@ const config = {
       },
     },
   },
-  plugins: [tailwindcssAnimate],
-} satisfies Config;
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
 
-export default config;
+export default config
+
