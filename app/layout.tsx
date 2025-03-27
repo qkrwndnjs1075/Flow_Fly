ba3dd8bbbdbb0992e071bd2747f324e5155059b8
박주원 <qkrwndnjs7308@gmail.com>
2025-03-27 16:19:13 +0900

    feat : 채널톡

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
-import type React from "react"
-import { Navbar } from "@/components/navbar"
-import { ThemeProvider } from "@/components/theme-provider"
-import { AuthProvider } from "@/lib/auth-context"
-import { Toaster } from "@/components/ui/toaster"
-import { Footer } from "@/components/footer"
-import "./globals.css"
+import type React from "react";
+import { Navbar } from "@/components/navbar";
+import { ThemeProvider } from "@/components/theme-provider";
+import { AuthProvider } from "@/lib/auth-context";
+import { Toaster } from "@/components/ui/toaster";
+import { Footer } from "@/components/footer";
+import AppProvider from "@/components/AppProvider"; // 절대 경로 사용 (상대경로 필요 시 변경)
+import "./globals.css";
 
 export const metadata = {
   title: "plob - 포트폴리오 & 블로그",
@@ -14,38 +15,29 @@ export const metadata = {
     apple:
       "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/%EC%8A%A4%ED%81%AC%EB%A6%B0%EC%83%B7%202025-03-19%20093722-RTDceE3SEwtLiPPRyY8LVNRwDIbXO0.png",
   },
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko" suppressHydrationWarning className="dark">
       <head>
-        <link
-          rel="icon"
-          href="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/%EC%8A%A4%ED%81%AC%EB%A6%B0%EC%83%B7%202025-03-19%20093722-RTDceE3SEwtLiPPRyY8LVNRwDIbXO0.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          href="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/%EC%8A%A4%ED%81%AC%EB%A6%B0%EC%83%B7%202025-03-19%20093722-RTDceE3SEwtLiPPRyY8LVNRwDIbXO0.png"
-        />
+        <link rel="icon" href={metadata.icons.icon} type="image/png" />
+        <link rel="apple-touch-icon" href={metadata.icons.apple} />
       </head>
       <body className="bg-[#0f0f18]">
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange>
           <AuthProvider>
-            <div className="relative flex min-h-screen flex-col">
-              <Navbar />
-              <main className="flex-1">{children}</main>
-              <Footer />
-            </div>
-            <Toaster />
+            <AppProvider>
+              <div className="relative flex min-h-screen flex-col">
+                <Navbar />
+                <main className="flex-1">{children}</main>
+                <Footer />
+              </div>
+              <Toaster />
+            </AppProvider>
           </AuthProvider>
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
-
