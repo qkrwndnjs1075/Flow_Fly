d586c5b7ed50ebd66e686c77b943ac185fff9374
박주원 <qkrwndnjs7308@gmail.com>
2025-03-29 23:22:01 +0900

    refactor :: add imageUrl and role

--- app/models/User.ts
+++ app/models/User.ts
@@ -4,13 +4,22 @@ interface IUser extends Document {
   email: string;
   password: string;
   name: String;
+  imageUrl: String;
   _id: mongoose.Types.ObjectId;
+  role: "user" | "admin";
 }
 
 const UserSchema: Schema<IUser> = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ["user", "admin"], // Mongoose enum 정의
+    default: "user",
+    required: true,
+  },
 });
 
 const User: Model<IUser> = mongoose.models.User || mongoose.model<IUser>("User", UserSchema);
